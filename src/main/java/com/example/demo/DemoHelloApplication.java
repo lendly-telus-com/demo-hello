93d26a82a0460f17173e43224fbdb7ec33d7a37c
Lendly Cagata <lendly.cagata@telus.com>
2023-02-08 20:09:48 +0800

    feat: initial commit

---
+++ src/main/java/com/example/demo/DemoHelloApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHelloApplication.class, args);
+		System.out.println("Hello Pogs");
+	}
+
+}
